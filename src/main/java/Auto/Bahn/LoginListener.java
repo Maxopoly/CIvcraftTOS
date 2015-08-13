70a969b5caa95e214033dfcc0c295738efc824c6
maxopoly <maxseinbot@gmx.de>
2015-08-13 16:31:42 +0200

    actual code

--- src/main/java/Auto/Bahn/LoginListener.java
+++ src/main/java/Auto/Bahn/LoginListener.java
@@ -1,5 +1,45 @@
 package Auto.Bahn;
 
-public class LoginListener {
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
+
+public class LoginListener implements Listener {
+	@EventHandler
+	public void playerJoinEvent(PlayerLoginEvent e) {
+		Player p = e.getPlayer();
+		if (p != null) {
+			if (!RegisteredConfig.registeredPlayers
+					.containsKey(p.getUniqueId())) {
+				showTOS(p);
+			}
+		}
+	}
+
+	public void showTOS(Player p) {
+		if (p == null) {
+			return;
+		}
+		TextComponent welcome = new TextComponent("Welcome to Civcraft!");
+		welcome.setColor(ChatColor.RED);
+		welcome.setBold(true);
+		p.spigot().sendMessage(welcome);
+		TextComponent agree = new TextComponent(
+				"To be able to play, you need to read our terms of service first and agree to those");
+		p.spigot().sendMessage(agree);
+		TextComponent click = new TextComponent(
+				"You can click this message to open up the terms of service");
+		click.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL,
+				"http://pornhub.com")); // TODO TODO TODO change to wherever the
+										// tos are
+		p.spigot().sendMessage(click);
+		TextComponent confirm = new TextComponent("Once you've read it, you can click this message to agree to the terms");
+		confirm.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/replace with right command"));  //TODO
+		p.spigot().sendMessage(confirm);
+	}
 }
--- src/main/java/Auto/Bahn/RegisteredConfig.java
+++ src/main/java/Auto/Bahn/RegisteredConfig.java
@@ -1,5 +1,104 @@
 package Auto.Bahn;
 
-public class RegisteredConfig {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.UUID;
 
+import org.bukkit.entity.Player;
+
+
+public class RegisteredConfig {
+	public static Map <UUID,Long> registeredPlayers;
+	public static File logFile;
+	
+	public RegisteredConfig() {
+		registeredPlayers = new TreeMap<UUID, Long>();
+		logFile = new File("registeredPlayers.txt");
+		parseLogFile();
+	}
+	
+	public void parseLogFile() {
+	        String line = null;
+	        try {
+	            FileReader fileReader = 
+	                new FileReader(logFile.getName());
+	            BufferedReader bufferedReader = 
+	                new BufferedReader(fileReader);
+
+	            while((line = bufferedReader.readLine()) != null) {
+	                parseLine(line);
+	            }    
+	            bufferedReader.close();            
+	        }
+	        catch(FileNotFoundException ex) {
+	            //TODO fix this so it works with main plugin
+	        	Logger.log("File not found, so no data about people who accepted the TOS before was created");
+	        }
+	        catch(IOException ex) {
+	            System.out.println(
+	                "Error reading file '" 
+	                + logFile.getName() + "'");    
+	            ex.printStackTrace();
+	        }
+	}
+	
+	public void parseLine(String line) {
+		String [] values = line.split(":");
+		if (values.length!=2) {
+			Logger.log("Found invalid line while loading registered players: "+line); //TODO
+			return;
+		}
+		UUID playerUUID = UUID.fromString(values[0]);
+		Long time = Long.parseLong(values[1]);
+		if (registeredPlayers!=null) {
+			registeredPlayers.put(playerUUID,time);
+		}
+	}
+	
+	public static void addPlayer(Player p) {
+		if (p==null) {
+			return;
+		}
+		UUID playerUUID= p.getUniqueId();
+		Long time = System.currentTimeMillis();
+		if (registeredPlayers != null) {
+			registeredPlayers.put(playerUUID,time);
+		}
+		
+		//write the name to logfile right away
+		writeToFile(playerUUID.toString()+":"+time.toString(),logFile);
+	}
+
+	public static void writeToFile(String content, File file) {
+		BufferedWriter writer = null;
+		try
+		{
+		    writer = new BufferedWriter( new FileWriter(file));
+		    writer.write(content+"\n");
+
+		}
+		catch ( IOException e)
+		{
+			//TODO log something
+		}
+		finally
+		{
+		    try
+		    {
+		        if ( writer != null)
+		        writer.close( );
+		    }
+		    catch ( IOException e)
+		    {
+		    	//TODO log something
+		    }
+		}
+	}
 }
